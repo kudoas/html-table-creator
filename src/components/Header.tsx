b54d0cbc7507126475d6941c146dcfe006159c0d
Daichi Kudo <sakanactor624@gmail.com>
2020-06-19 22:57:17 +0900

    :lipstick: Modified Contact Form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 const Header: React.FCX = ({ className }) => {
   return (
     <header className={className}>
       <nav>
-        <h1>HTML Table Creator</h1>
+        <h1>
+          <Link to="/">HTML Table Creator</Link>
+        </h1>
       </nav>
     </header>
   );
 };
 
-export default Header;
+const StyledHeader = styled(Header)`
+  h1 {
+    padding: 10px;
+  }
+  a {
+    color: inherit;
+  }
+`;
+
+export default StyledHeader;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { GlobalCSS, ResetCSS } from '../styles';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -8,8 +7,6 @@ import Footer from './Footer';
 const App: React.FC = () => {
   return (
     <>
-      <ResetCSS />
-      <GlobalCSS />
       <Header />
       <Main />
       <Footer />
