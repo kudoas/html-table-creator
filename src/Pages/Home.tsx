99bac84c246348285cd716c71a341e18b64bebbb
Daichi Kudo <sakanactor624@gmail.com>
2020-06-20 22:04:36 +0900

    :lipstick: Add favicons

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
 
 const App: React.FC = () => {
   return (
