3a604c41b93791b9a2af76fc1361eedd6a02e44e
Daichi Kudo <sakanactor624@gmail.com>
2020-10-27 08:10:11 +0900

    DOM層とContainer層の分離

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from '@emotion/styled';
 
 import useNewTable from '../hooks/useNewTable';
 import Header from '../components/Header';
@@ -6,7 +7,57 @@ import ControllPanel from '../components/Controllpanel/ControllPanel';
 import Footer from '../components/Footer';
 import Output from '../components/Controllpanel/Output/Output';
 
-const Container: React.FCX = ({ className }) => {
+type Props = {
+  isPreview: boolean;
+  state: string[][];
+  onClick: () => void;
+  onChange: (e: React.FormEvent<HTMLInputElement>, column: number, row: number) => void;
+  addColumn: () => void;
+  removeColumn: () => void;
+  addRow: () => void;
+  removeRow: () => void;
+  deleteAllItems: () => void;
+  reset: () => void;
+};
+
+const Component: React.FCX<Props> = ({
+  className,
+  isPreview,
+  state,
+  onClick,
+  onChange,
+  addColumn,
+  removeColumn,
+  addRow,
+  removeRow,
+  deleteAllItems,
+  reset,
+}) => (
+  <div className={className}>
+    <Header />
+    <h2>Create your table</h2>
+    <button onClick={onClick}>切り替え</button>
+    {isPreview ? (
+      <Output state={state} />
+    ) : (
+      <ControllPanel
+        state={state}
+        onChange={onChange}
+        addColumn={addColumn}
+        removeColumn={removeColumn}
+        addRow={addRow}
+        removeRow={removeRow}
+        deleteAllItems={deleteAllItems}
+        reset={reset}
+      />
+    )}
+    <Footer />
+  </div>
+);
+
+const StyledComponent = styled(Component)``;
+
+const Container: React.FCX<Props> = () => {
   const [isPreview, setIsPreview] = useState(false);
 
   const {
@@ -21,26 +72,18 @@ const Container: React.FCX = ({ className }) => {
   } = useNewTable();
 
   return (
-    <>
-      <Header />
-      <h2>Create your table</h2>
-      <button onClick={() => setIsPreview(!isPreview)}>切り替え</button>
-      {isPreview ? (
-        <Output state={state} />
-      ) : (
-        <ControllPanel
-          state={state}
-          onChange={onChange}
-          addColumn={addColumn}
-          removeColumn={removeColumn}
-          addRow={addRow}
-          removeRow={removeRow}
-          deleteAllItems={deleteAllItems}
-          reset={reset}
-        />
-      )}
-      <Footer />
-    </>
+    <StyledComponent
+      state={state}
+      onChange={onChange}
+      addColumn={addColumn}
+      removeColumn={removeColumn}
+      addRow={addRow}
+      removeRow={removeRow}
+      deleteAllItems={deleteAllItems}
+      reset={reset}
+      isPreview={isPreview}
+      onClick={() => setIsPreview(!isPreview)}
+    />
   );
 };
 
