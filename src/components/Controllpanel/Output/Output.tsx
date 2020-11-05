e50c92d6298d65e5d6adcd086772f84bb7398791
Daichi Kudo <sakanactor624@gmail.com>
2020-11-05 21:31:12 +0900

    refactoring output

--- src/components/Controllpanel/Output/Output.tsx
+++ src/components/Controllpanel/Output/Output.tsx
@@ -58,37 +58,4 @@ const Container: React.FCX<Props> = ({ state }) => {
   return <StyledComponent state={state} isCopied={isCopied} onCopy={() => setIsCopied(true)} />;
 };
 
-const StyledContainer = styled(Container)`
-  height: 500px;
-  pre {
-    background-color: #e0e5ec;
-    padding: 16px;
-    box-shadow: 2px 2px 10px #ffffff, -2px -2px 10px #a3b1c6;
-    border-radius: 5px;
-    height: 400px;
-  }
-  > div {
-    text-align: center;
-    margin: 10px 10px;
-  }
-  button {
-    padding: 10px;
-    background-color: #e0e5ec;
-    box-shadow: 1px 1px 5px #a3b1c6, -1px -1px 5px #ffffff;
-    margin: 10px;
-    border-radius: 4px;
-    color: black;
-    &:hover {
-      color: white;
-    }
-  }
-
-  /* os: dark mode or not */
-  /* will storage dark mode or not to localStorage */
-  /* @media (prefers-color-scheme: dark) {
-    background-color: dimgray;
-    color: #fff;
-  } */
-`;
-
-export default StyledContainer;
+export default Container;
