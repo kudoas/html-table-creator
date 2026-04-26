5bf829f4b1843e5a25d9ddd8d37d9b3acb191daf
Daichi <sakanactor624@gmail.com>
2026-04-26 10:12:16 +0900

    Add test coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/*.spec.{ts,tsx}',
+    '!src/index.tsx',
+    '!src/types.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
 };
